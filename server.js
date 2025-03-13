acef7ded1bacb9c3ecc6d8b5ed4f0d942a0464a1
Pedro <pedroesnarriaga@gmail.com>
2025-03-13 15:34:40 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import http from "http";
+
+const PORT = 3000;
+
+const routes = {
+    "/": "Hello World!",
+    "/test": "Test"
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end(routes[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("Server running at http://localhost:3000/");
+});
