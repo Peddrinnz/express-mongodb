3eba42caf02e56865c0a6e104f6a6e0dbaf2c326
Pedro <pedroesnarriaga@gmail.com>
2025-03-14 14:31:17 -0400

    feature: CRUD complete

--- src/app.js
+++ src/app.js
@@ -16,10 +16,43 @@ app.get("/books", (req, res) => {
     res.status(200).json(books);
 });
 
+app.get("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const book = books.find((b) => b.id === id);
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).send("Book not found");
+    }
+});
+
 app.post("/books", (req, res) => {
     const newBook = req.body;
     books.push(newBook);
     res.status(201).json(newBook);
+});
+
+app.put("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex((b) => b.id === id);
+
+    if (bookIndex !== -1) {
+        books[bookIndex] = { ...books[bookIndex], ...req.body };
+        res.status(200).json(books[bookIndex]);
+    } else {
+        res.status(404).send("Book not found");
+    }
+});
+
+app.delete("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex((b) => b.id === id);
+    if (bookIndex !== -1) {
+        books.splice(bookIndex, 1);
+        res.status(200).send("Book deleted");
+    } else {
+        res.status(404).send("Book not found");
+    }
 });
 
 export default app;
