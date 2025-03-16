c4f707b75e7e08d0bbf8eb6a0e4966c248b84a36
Pedro <pedroesnarriaga@gmail.com>
2025-03-16 12:12:09 -0400

    feature: CRUD completed

--- src/app.js
+++ src/app.js
@@ -1,43 +1,11 @@
 import express from "express";
 import connect from "./config/dbConnect.js";
-import Book from "./models/book.js";
+import routes from "./routes/index.js";
 
 const connection = await connect();
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
-});
-
-app.get("/books/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const book = books.find((b) => b.id === id);
-    if (book) {
-        res.status(200).json(book);
-    } else {
-        res.status(404).send("Book not found");
-    }
-});
-
-app.post("/books", (req, res) => {
-    const newBook = req.body;
-    books.push(newBook);
-    res.status(201).json(newBook);
-});
-
-app.put("/books/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const bookIndex = books.findIndex((b) => b.id === id);
-
-    if (bookIndex !== -1) {
-        books[bookIndex] = { ...books[bookIndex], ...req.body };
-        res.status(200).json(books[bookIndex]);
-    } else {
-        res.status(404).send("Book not found");
-    }
-});
+routes(app);
 
 app.delete("/books/:id", (req, res) => {
     const id = parseInt(req.params.id);
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,8 +2,51 @@ import Book from "../models/book.js";
 
 class BookController {
     static async listBooks(req, res) {
-        const listBooks = await Book.find({});
-        res.status(200).json(listBooks);
+        try {
+            const listBooks = await Book.find({});
+            res.status(200).json(listBooks);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    static async listBookForId(req, res) {
+        try {
+            const id = req.params.id;
+            const findBook = await Book.findById(id);
+            res.status(200).json(findBook);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    static async createBook(req, res) {
+        try {
+            const newBook = await Book.create(req.body);
+            res.status(201).json({message: "Book created",newBook});
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+
+    static async updateBook(req, res) {
+        try {
+            const id = req.params.id;
+            await Book.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "Book updated"});
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    }
+    
+    static async deleteBook(req, res) {
+        try {
+            const id = req.params.id;
+            await Book.findByIdAndDelete(id);
+            res.status(200).json({message: "Book deleted"});
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 };
 
